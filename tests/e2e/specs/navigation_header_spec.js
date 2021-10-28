65115f5fab4492485da37209676ecf2d3831eeb3
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-28 17:17:20 +0200

    Correct assertion

--- tests/e2e/specs/navigation_header_spec.js
+++ tests/e2e/specs/navigation_header_spec.js
@@ -5,7 +5,7 @@ describe("the navigation header", () => {
 
   it("lets the user navigate to the 'Houses' page and shows which page is active", () => {
     cy.get(".nav").contains("Houses").click();
-    cy.url().should("eq", "/");
+    cy.url().should("eq", "http://localhost:8080/");
     cy.get(".nav")
       .contains("Houses")
       .should("have.class", "router-link-exact-active");
