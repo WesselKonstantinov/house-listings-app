4b9f8b6d325dccdd1732c4b83305bf018d46f159
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-30 17:20:00 +0200

    Correct unit test to account for routing

--- tests/unit/mobile_navigation_bar.spec.js
+++ tests/unit/mobile_navigation_bar.spec.js
@@ -1,16 +1,25 @@
 import { shallowMount } from "@vue/test-utils";
 import MobileNavigationBar from "@/components/MobileNavigationBar.vue";
-import HomeIcon from "../../src/assets/ic_mobile_navigarion_home.png";
+import HomeIconActive from "../../src/assets/ic_mobile_navigarion_home_active.png";
 
 describe("MobileNavigationBar.vue", () => {
-  const wrapper = shallowMount(MobileNavigationBar);
+  const wrapper = shallowMount(MobileNavigationBar, {
+    global: {
+      mocks: {
+        $route: {
+          path: "/",
+          name: "Home",
+        },
+      },
+    },
+  });
   it("renders a navigation section element", () => {
     const nav = wrapper.find("nav");
     expect(nav.exists()).toBeTruthy();
   });
 
-  it("renders a link with a house icon", () => {
+  it("renders a link with an active house icon", () => {
     const navItem = wrapper.findAll(".nav__icon").at(0);
-    expect(navItem.attributes("src")).toEqual(HomeIcon);
+    expect(navItem.attributes("src")).toEqual(HomeIconActive);
   });
 });
