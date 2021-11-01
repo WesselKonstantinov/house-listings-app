4dd885f05dbeb2f601ec8ec5a0fae812d05f3d03
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-11-01 13:39:19 +0100

    Configure store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,37 @@
+import axios from "axios";
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    houseListings: [],
+    isFetching: false,
+  },
+  mutations: {
+    setHouseListings(state, { houseListings }) {
+      state.houseListings = houseListings;
+    },
+    setIsFetching(state, bool) {
+      state.isFetching = bool;
+    },
+  },
+  actions: {
+    getHouseListings({ commit }) {
+      commit("setIsFetching", true);
+      return axios
+        .get(process.env.VUE_APP_API_URL, {
+          headers: {
+            "X-Api-Key": process.env.VUE_APP_API_KEY,
+          },
+        })
+        .then((response) => {
+          commit("setIsFetching", false);
+          commit("setHouseListings", { houseListings: response.data });
+        })
+        .catch((error) => {
+          commit("setIsFetching", false);
+          console.error(error);
+        });
+    },
+  },
   modules: {},
 });
