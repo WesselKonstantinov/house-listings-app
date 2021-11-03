2db23d6d7186d9a863b1a5a52f82bb6019d6f3f5
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-11-03 17:44:33 +0100

    Rewrite e2e test for homepage

--- tests/e2e/specs/home_page_spec.js
+++ tests/e2e/specs/home_page_spec.js
@@ -3,5 +3,7 @@ describe("The home page", () => {
     cy.visit("/");
   });
 
-  it("shows a list of all available houses", () => {});
+  it("shows a list of all available houses", () => {
+    cy.get(".home-page__content > article").should("be.visible");
+  });
 });
