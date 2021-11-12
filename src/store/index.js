a204c2eb7cd5f1d1a438d94f7d1c7c336f2a0e25
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-11-12 14:33:29 +0100

    Update store with operations for a single house listing

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,25 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     houseListings: [],
+    selectedHouseListingId: null,
     isFetching: false,
   },
   mutations: {
     setHouseListings(state, { houseListings }) {
       state.houseListings = houseListings;
     },
+    setSelectedHouseListing(state, id) {
+      state.selectedHouseListingId = id;
+    },
     setIsFetching(state, bool) {
       state.isFetching = bool;
     },
+  },
+  getters: {
+    selectedHouseListing: (state) =>
+      state.houseListings.find(
+        (houseListing) => houseListing.id === state.selectedHouseListingId
+      ),
   },
   actions: {
     getHouseListings({ commit }) {
