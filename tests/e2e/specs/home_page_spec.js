22c06deacb451601628e4091fc46c4a0ecc2788b
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-11-14 22:10:36 +0100

    Add additional test

--- tests/e2e/specs/home_page_spec.js
+++ tests/e2e/specs/home_page_spec.js
@@ -6,4 +6,10 @@ describe("The home page", () => {
   it("shows a list of all available houses", () => {
     cy.get(".home-page__content > article").should("be.visible");
   });
+
+  it("lets the user access the details of a house by clicking on a listing", () => {
+    cy.get(".home-page__content > article").first().click();
+    cy.url().should("include", "/house-listings/2");
+    cy.url().should("eq", "http://localhost:8080/house-listings/2");
+  });
 });
