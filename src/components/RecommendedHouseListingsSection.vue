7bd48f0d4b1de2dd6a77b3f73cedab0766db1300
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-11-16 12:38:32 +0100

    Create recommended section

---
+++ src/components/RecommendedHouseListingsSection.vue
@@ -0,0 +1,64 @@
+<template>
+  <aside class="recommendations" v-if="areRecommendedHouseListingsPresent">
+    <h3>Recommended for you</h3>
+    <house-listing-card
+      v-for="houseListing in recommendedHouseListingsByLocation"
+      :key="houseListing.id"
+      :house-listing="houseListing"
+      @click="onChangeHouseListingDetailRoute(houseListing)"
+    />
+  </aside>
+</template>
+
+<style>
+.recommendations {
+  width: 90%;
+  margin: 0 auto;
+}
+
+.recommendations > h3 {
+  font-size: 14px;
+  margin-top: 0;
+}
+
+@media screen and (min-width: 768px) {
+  .recommendations {
+    width: 100%;
+  }
+
+  .recommendations > h3 {
+    font-size: 18px;
+    margin-top: 20px;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .recommendations > h3 {
+    margin-top: 0;
+  }
+}
+</style>
+
+<script>
+import { mapGetters } from "vuex";
+import HouseListingCard from "./HouseListingCard.vue";
+
+export default {
+  components: { HouseListingCard },
+  name: "RecommendedHouseListingsSection",
+  computed: {
+    ...mapGetters(["recommendedHouseListingsByLocation"]),
+    areRecommendedHouseListingsPresent() {
+      return this.recommendedHouseListingsByLocation.length !== 0;
+    },
+  },
+  methods: {
+    onChangeHouseListingDetailRoute(houseListing) {
+      this.$router.push({
+        name: "HouseListingDetail",
+        params: { id: houseListing.id },
+      });
+    },
+  },
+};
+</script>
