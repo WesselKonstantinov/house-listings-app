bd6b39cd9c2dcbb772904a4127e023d036b2b5b3
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-11-28 15:10:59 +0100

    Use one component for create and edit page

---
+++ src/views/CreateOrEditHouseListing.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <house-listing-form :house-listing="selectedHouseListing" />
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapGetters } from "vuex";
+import HouseListingForm from "../components/HouseListingForm.vue";
+
+export default {
+  name: "CreateOrEditHouseListing",
+  components: { HouseListingForm },
+  computed: {
+    ...mapGetters(["selectedHouseListing"]),
+  },
+  methods: {
+    ...mapMutations(["setSelectedHouseListing"]),
+  },
+  created() {
+    if ("id" in this.$route.params) {
+      const houseListingId = Number(this.$route.params.id);
+      this.setSelectedHouseListing(houseListingId);
+    } else {
+      this.setSelectedHouseListing(null);
+    }
+  },
+};
+</script>
