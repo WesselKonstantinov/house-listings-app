a7c163e5afd5f7e55d9d4eb750d9b938b2886c4a
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-12-06 20:40:27 +0100

    Pass store reference to Cypress

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Get store reference during E2E tests
+if (window.Cypress) {
+  window.__store__ = store;
+}
+
 createApp(App).use(store).use(router).mount("#app");
